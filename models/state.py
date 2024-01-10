af639fa7650e1db576a795f5b31c18bddb38b37f
m-mwangi <m.mwangi2@alustudent.com>
2024-01-10 13:49:55 -0800

    state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Represents a state
+    Attributes:
+        name (str): The name
+    """
+    name = ""
